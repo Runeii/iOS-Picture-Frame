bb0fa7d5b8ba4df3b0809734c9539e63f1c188f1
Andrew Hill <contact@andrewthomashill.co.uk>
2024-09-09 11:18:29 +0200

    Initial Commit

---
+++ DigitalPictureFrame/DigitalPictureFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalPictureFrameApp.swift
+//  DigitalPictureFrame
+//
+//  Created by Andrew Hill on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct DigitalPictureFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
